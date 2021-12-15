2da736924dc1b9c1980c29e531f3de63dd553ffe
Adam Dobrowolski <adam.dobrowolski@mudita.com>
2021-12-15 18:21:35 +0100

    master it works

--- anal.py
+++ anal.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 
+from __future__ import annotations
 from dataclasses import dataclass
-import re
 import json
 import plotly.express as px
+from typing import Union, List
+from treelib import Tree, Node
+
 
 def load():
     with open("lol.json") as f:
@@ -11,7 +14,7 @@ def load():
 
 
 @dataclass
-class entry:
+class Entry:
     name: str
     cat: str
     ph: str
@@ -22,39 +25,61 @@ class entry:
     args: dict
 
 
-@dataclass
-class group:
-    name: str
-    values: list[entry]
-    timetotal: int
+class Data:
+    def __init__(self):
+        self.data: Union[Entry, int, None] = 0
+
+    def setData(self, data):
+        if type(data) not in [Entry, int, None]:
+            raise RuntimeError(f"data should be either Entry, int or None, but is {type(data)}")
+        self.data = data
 
 
 if __name__ == "__main__":
-    timings = {}
-    regexp = re.compile(r"^module-(apps|services)/([A-Za-z\-]*)/.*")
+    root = Tree()
+    total = root.create_node('total', 'total', data=Data())
 
+    def keygen(x: List[str], y):
+        return '.'.join(x[:y + 1])
+
+    # load targets to nodes
     for val in load():
-        el = entry(**val)
+        el = Entry(**val)
         if ' ' in el.name:
             # WIP ignore shitty targets with ` ` in name
             continue
-        name = regexp.match(el.name)
-        if name is not None:
-            g = name.group(2)
-            if g not in timings:
-                timings[g] = group(g, [], 0)
-            timings[g].values.append(el)
-            timings[g].timetotal = timings[g].timetotal + el.dur
-    for key, val in sorted(timings.items(), key=lambda x: x[1].timetotal):
-        print(f"{key} : {val.timetotal/1000000 : ^16}")
-        for el in sorted(val.values, key=lambda x: x.dur):
-            print(" " * 10 + f"{el.name.split('/')[-1]} time: {el.dur/1000000}")
 
-    data = dict(
-        character=["Eve", "Cain", "Seth", "Enos", "Noam", "Abel", "Awan", "Enoch", "Azura"],
-        parent=["", "Eve", "Eve", "Seth", "Seth", "Eve", "Eve", "Awan", "Eve" ],
-        value=[10, 14, 12, 10, 2, 6, 6, 4, 4])
-    fig =px.sunburst(
+        branches = el.name.split('/')
+        previous = total
+
+        for key, val in enumerate(branches):
+            id = keygen(branches, key)
+            if id in root:
+                previous = root[id]
+            else:
+                previous = root.create_node(val, id, parent=previous, data=Data())
+            if type(root[id].data.data) is Entry:
+                print("something is wrong on {val} : {branches} : {el.name}")
+            else:
+                root[id].data.data = root[id].data.data + el.dur
+
+    # print(root.to_json(with_data=False))
+
+    data = {"character": [], "parent": [], "value": []}
+    for node in root.expand_tree():
+        node = root[node]
+        character = node.identifier
+        pre = root.parent(node.identifier)
+        parent = pre.identifier if pre is not None else ""
+        value = node.data.data if type(node.data.data) is int else node.data.data.dur
+        # print("------------------------------")
+        # print(f": {character} : {parent} : {value}")
+        # print("------------------------------")
+        data["character"].append(character)
+        data["parent"].append(parent)
+        data["value"].append(value)
+
+    fig = px.sunburst(
         data,
         names='character',
         parents='parent',
